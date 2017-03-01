59e5573cae9d90dcbe83632ea9bbfc5070280dc4
downtownalexbrown <alex.brown9@pcc.edu>
2017-03-01 14:49:42 -0800

    Changed graphing to an ellipse

--- sphere.py
+++ sphere.py
@@ -1,21 +1,28 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import time
-
-ball = plt.Circle((5, 5), 1, alpha=0.55, color='blue')
+from matplotlib.patches import Ellipse
 
 # Initilize the graph and stuff
-ax = plt.subplot(aspect='equal')
+fig = plt.figure()
+fig.set_dpi(100)
+ax = plt.subplot(xlim=(0,10), ylim=(0,10))
+
+# Fancy geometry stuff to scale the width and height of the ellipse
+x0, y0 = ax.transAxes.transform((0, 0)) # lower left in pixels
+x1, y1 = ax.transAxes.transform((10, 10)) # upper right in pixes
+dx = x1 - x0
+dy = y1 - y0
+maxd = max(dx, dy)
+width = 1 * maxd / dx
+height = 1 * maxd / dy
+
+# Add the ball object
+ax.add_artist(Ellipse((2, 2), width, height, 1))
+
+#def animate():
+ball = plt.Circle((7, 7), 1, alpha=0.55, color='blue')	
 ax.add_artist(ball)
-
-# Change size of graph
-ax.set_xlim((0, 10))
-ax.set_ylim((0, 10))
-
-def animate():
-	ball = plt.Circle((5, 5), 1, alpha=0.55, color='blue')	
-
-ani = animation.FuncAnimation(fig, animate, interval = 2)
+#ani = animation.FuncAnimation(fig, animate, interval = 2)
 
 plt.show()
 
