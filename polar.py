835602ef7564cc9273adf7fbcd56ca28677b30a2
downtownalexbrown <alex.brown9@pcc.edu>
2017-03-20 15:01:36 -0700

    Added labels and the Sun as a focu

--- polar.py
+++ polar.py
@@ -10,6 +10,9 @@ c = 2.5 # Distance from center to foci
 theta = 0
 points = []
 
+fig = plt.figure()
+ax = plt.subplot(111, projection='polar')
+
 while theta < 2*np.pi: # Completes one orbit
         r = (a*(1-e**2))/(1+e*np.cos(theta))
 #        print(np.c_[r,theta])
@@ -20,10 +23,30 @@ while theta < 2*np.pi: # Completes one orbit
 	y = b * np.sin(theta)
 	print(np.c_[x, y]) # x = acos(t), y = bsin(t)
 
+plt.polar(*zip(*points))
+ax.scatter(theta, c, cmap='hsv', alpha=0.75)
+ax.annotate('Sun (2.5, 0)',
+            xy=(0, c),  # theta, radius
+            xytext=(0.525, 0.47),    # fraction, fraction
+            textcoords='figure fraction',
+            horizontalalignment='left',
+            verticalalignment='bottom')
+
+ax.annotate('Eccentricity: 0.0167',
+            xy=(0, c),  # theta, radius
+            xytext=(0.05, 0.05),    # fraction, fraction
+            textcoords='figure fraction',
+            horizontalalignment='left',
+            verticalalignment='bottom')
+
+earth = ax.scatter(points[0], cmap='hsv', alpha = 0.75, color='green')
+
+#for coordinate in points:
+	# points[0] is (0, 147.11966578236201). I need to split the string to get each point, then apply that to the scatter function
+	
 
 if __name__ == '__main__':
         if len(sys.argv) > 1 and sys.argv[1] == 'save':
                 plt.savefig('plot.jpg', dpi=80, writer='imagemagick')
         else:
-		plt.polar(*zip(*points))
                 plt.show()
